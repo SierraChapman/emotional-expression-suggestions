c3d63dffce52dc04d9c3928dd80f085e4dcf9c28
Aidan Sweeny <apswee22@colby.edu>
2020-07-01 16:59:54 -0400

    Added some personal query

--- game-script.js
+++ game-script.js
@@ -1,16 +1,28 @@
 $("#gameSearch").on("click", function(event){
-    var dates = ("#title").val();
-    if (("#genres").val() !== ""){
-        $.ajax({
-            url: "https://api.rawg.io/api/genres?ordering=-added",
-            method: "GET"
-        }).then(function(response) {
-            console.log(response);
-        });
+    event.preventDefault();
+    var title = $("#title").val();
+    var genre = $("#genre").val()
+    var firstDate = $("#firstDate").val()
+    var secondDate = $("#secondDate").val()
+    var genre = $("#genre").val()
+    var queryURL = "https://api.rawg.io/api/games?ordering=-added"
+    if (genre !== "None" || genre !== "Genre"){
+        queryURL += "&genre=" + genre;
     }
-    
+    if (title !== ""){
+        queryURL += "&search=" + title;
+    }
+    if (firstDate !== "" && secondDate !== ""){
+        queryURL += "&dates=" + firstDate + "," + secondDate;
+    }
+    else if (firstDate !== ""){
+        queryURL += "&dates=" + firstDate + ",2020-07-01";
+    }
+    else if (secondDate !== ""){
+        queryURL += "&dates=1950-01-01," + secondDate;
+    }
     $.ajax({
-        url: "https://api.rawg.io/api/games?dates=" + dates + "&ordering=-added",
+        url: queryURL,
         method: "GET"
     }).then(function(response) {
 		console.log(response);
