7c910cd69deb4bc7dd885536341b738673b1eca0
Aidan Sweeny <apswee22@colby.edu>
2020-07-05 14:51:04 -0400

    Added next and previous button

--- game-script.js
+++ game-script.js
@@ -1,5 +1,53 @@
+var buttons = false;
+var nextURL = "";
+var prevURL = "";
+var queryURL = "https://api.rawg.io/api/games?ordering=relevance";
+
+function queryCall(){
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        $("#results").empty();
+        console.log(response);
+        for(var i=0; i<response.results.length; i++){ 
+            var div = $("<div>");
+            var gameTitle = $("<h2>");
+            gameTitle.text(response.results[i].name);
+            var poster = $("<img>");
+            poster.css("width", "200px");
+            poster.attr("src", response.results[i].background_image);
+            var gameGenre = $("<ul>");
+            if(response.results[i].genres.length > 0){
+                gameGenre.text("Genres: ");
+                for(var j=0; j<response.results[i].genres.length; j++){
+                    var li = $("<li>");
+                    li.text(response.results[i].genres[j].name);
+                    gameGenre.append(li);
+                }
+            }
+            var gamePlatforms = $("<ul>");
+            if(response.results[i].platforms !== null){
+                gamePlatforms.text("Platforms: ");
+                for(var j=0; j<response.results[i].platforms.length; j++){
+                    var li = $("<li>");
+                    li.text(response.results[i].platforms[j].platform.name);
+                    gamePlatforms.append(li);
+                }
+            }
+            var releaseDate = $("<p>");
+            var rating = $("<p>");
+            rating.text("Rating: " + response.results[i].rating);
+            releaseDate.text("Release Date: " + response.results[i].released);
+            div.append(gameTitle, poster, gameGenre, gamePlatforms, releaseDate, rating);
+            $("#results").append(div);
+        }
+        nextURL = response.next
+        prevURL = response.previous
+    });
+}
 $("#gameSearch").on("click", function(event){
-    console.log("yes");
     event.preventDefault();
     var title = $("#title").val();
     var genre = $("#genre").val()
@@ -8,9 +56,10 @@ $("#gameSearch").on("click", function(event){
     var genre = $("#genre").val()
     var resultsNum = $("#resultsNum").val();
     var searchBy = $("#searchBy").val();
-    var queryURL = "https://api.rawg.io/api/games?ordering=relevance"
-    if (genre !== "None" || genre !== "Genre"){
-        queryURL += "&genre=" + genre;
+    queryURL = "https://api.rawg.io/api/games?ordering=relevance";
+    if (genre !== "Genre"){
+        genre = genre.replace(" ", "-");
+        queryURL += "&genres=" + genre.toLowerCase();
     }
     if (title !== ""){
         title = title.replace(" ", "+");
@@ -47,63 +96,30 @@ $("#gameSearch").on("click", function(event){
         queryURL += "&ordering=released";
     }
     else if (searchBy == "Rating"){
-        queryURL += "&ordering=rating3";
+        queryURL += "&ordering=-rating";
     }
     console.log(queryURL);
-    var numGames = 0;
-    console.log(resultsNum);
-    // while (numGames < resultsNum){
-    console.log(numGames);
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        $("#results").empty();
-        console.log(response);
-        for(var i=0; i<response.results.length; i++){ 
-            var div = $("<div>");
-            var gameTitle = $("<h2>");
-            gameTitle.text(response.results[i].name);
-            var poster = $("<img>");
-            poster.css("width", "200px");
-            poster.attr("src", response.results[i].background_image);
-            var gameGenre = $("<ul>");
-            if(response.results[i].genres.length > 0){
-                gameGenre.text("Genres: ");
-                for(var j=0; j<response.results[i].genres.length; j++){
-                    var li = $("<li>");
-                    li.text(response.results[i].genres[j].name);
-                    gameGenre.append(li);
-                }
-            }
-            var gamePlatforms = $("<ul>");
-            if(response.results[i].platforms !== null){
-                gamePlatforms.text("Platforms: ");
-                for(var j=0; j<response.results[i].platforms.length; j++){
-                    var li = $("<li>");
-                    li.text(response.results[i].platforms[j].platform.name);
-                    gamePlatforms.append(li);
-                }
-            }
-            var releaseDate = $("<p>");
-            releaseDate.text(response.results[i].released);
-            div.append(gameTitle, poster, gameGenre, gamePlatforms, releaseDate);
-            // if(response.results[i].released !== null){
-            //     releaseDate.text(response.results[i].released);
-            //     div.append(gameTitle, poster, gameGenre, gamePlatforms, releaseDate);
-            // }
-            // else {
-            //     numGames += 1;
-            // }
-            $("#results").append(div);
-            // if(numGames >= resultsNum){
-            //     break;
-            // }
-        }
-        if (numGames < resultsNum){
-            queryURL = results.next;
-        }
-    });
-    // }
+    queryCall();
+    if (buttons === false){
+        var next = $("<button>");
+        next.text("Next Page");
+        next.attr("class", "nextPage");
+        var prev = $("<button>");
+        prev.text("Previous Page");
+        prev.attr("class", "prevPage");
+        $("#pages").append(prev, next);
+        buttons = true;
+    }
+})
 
+$("#pages").on("click", "button.nextPage", function(event){
+    queryURL = nextURL;
+    window.scrollTo(0, 0);
+    queryCall();
+})
+
+$("#pages").on("click", "button.prevPage", function(event){
+    queryURL = prevURL;
+    window.scrollTo(0, 0);
+    queryCall();
 })
